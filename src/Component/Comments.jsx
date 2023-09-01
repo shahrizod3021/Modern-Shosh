167be3dbf1956d666c0ab6c42bee4da7ab8f84ba
shahrizod3021 <shahrizodmirzaaliyevdev@gmail.com>
2023-09-01 10:09:11 +0500

    28  commit

--- src/Component/Comments.jsx
+++ src/Component/Comments.jsx
@@ -36,7 +36,7 @@ export const Comments = () => {
             </div>
 
             <div className={"media comments"}>
-                <div className={"playfair comment-heading"} style={{letterSpacing: "1px"}}>
+                <div className={"playfair"} style={{letterSpacing: "1px"}}>
                     <h1 className={"text-white text-center"}>{localStorage.getItem("lang") === "uzb" ? "Odamlar boshqa odamlarga ishonadilar" : localStorage.getItem("lang") === "rus" ? "Люди верят другим людям" : "People Believe Other People "}</h1>
                     <p className={"text-center text-white "}>{localStorage.getItem("lang") === "uzb" ? "Mehmonlaringizga baxtli mijozlaringiz haqida aytib bering" : localStorage.getItem("lang") === "rus" ? "Расскажите своим посетителям о ваших счастливых клиентах" : "Let yout visitors know about your happy customers"}</p>
                 </div>
@@ -44,18 +44,18 @@ export const Comments = () => {
                     <div className={"col-md-12 row"}>
                         <div className={"col-md-4"}>
                             <p className={"text-white"}>Отзыв: Пребывание в гостинице ModernShosh было приятным опытом. Очень впечатлен уровнем обслуживания и профессионализмом персонала. Внимательное отношение к деталям и быстрое реагирование на запросы сделали мой отдых комфортным и беззаботным.</p>
-                            <h5 className={"text-white text-center lato pt-4"}>Отзыв 1:</h5>
+                            <h5 className={"text-white text-center lato pt-4"}>John Adam :</h5>
                             <p className={"text-center lato text-white"}>{localStorage.getItem("lang") === "uzb" ? "Qoniqarli mijoz" : localStorage.getItem("lang") === "rus" ? "Довольный клиент" : "Satisfied customer"}</p>
                         </div>
                         <div className={"col-md-4"}>
                             <p className={"text-white"}>Отзыв: Очень доволен услугами гостиницы ModernShosh. Вежливый и отзывчивый персонал всегда готов помочь. Номера чистые и удобные, уборка проводилась своевременно. Завтраки в ресторане были вкусными и разнообразными.</p>
-                            <h5 className={"text-white text-center lato pt-4"}>Отзыв 2:</h5>
+                            <h5 className={"text-white text-center lato pt-4"}>Lyudmila Safaeva :</h5>
                             <p className={"text-center lato text-white"}>{localStorage.getItem("lang") === "uzb" ? "Qoniqarli mijoz" : localStorage.getItem("lang") === "rus" ? "Довольный клиент" : "Satisfied customer"}</p>
 
                         </div>
                         <div className={"col-md-4"}>
                             <p className={"text-white"}>Отзыв: Хочу выразить благодарность за отличное обслуживание в гостинице ModernShosh. Все мои просьбы и вопросы решались оперативно. Уровень сервиса на высоте, персонал профессиональный и внимательный.</p>
-                            <h5 className={"text-white text-center lato pt-4 "}>Отзыв 3:</h5>
+                            <h5 className={"text-white text-center lato pt-4 "}>Murad Ivanov :</h5>
                             <p className={"text-center lato text-white"}>{localStorage.getItem("lang") === "uzb" ? "Qoniqarli mijoz" : localStorage.getItem("lang") === "rus" ? "Довольный клиент" : "Satisfied customer"}</p>
                         </div>
                     </div>
