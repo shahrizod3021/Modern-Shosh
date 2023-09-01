0cfe7619396edeab4f20ca3ce26a5a1f4233059c
shahrizod3021 <shahrizodmirzaaliyevdev@gmail.com>
2023-09-01 08:48:08 +0500

    24  commit

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -2,55 +2,58 @@ import logo from "../assets/img/logo big.png";
 
 export const Footer = () => {
     return (
-        <div>
-            <div className={"desktop-nav m-0 w-100 row col-md-12"}>
-                <div className={"col-md-2 mt-3"}>
-                    <a href="#home"><img src={logo} alt="topilmadi" width={"50%"} style={{marginLeft: "100px"}}
-                                    height={"100px"}/>
-                    </a>
-                </div>
-                <div className={"col-md-8"}>
-                    <div className={"d-flex align-items-center mt-4 flex-column  justify-content-center"}>
-                        <div className={"d-flex align-items center justify-content-center"}>
-                            <div className={"d-flex align-items-center flex-column mt-3"}
-                                 style={{marginRight: "250px"}}>
-                                <div>
-                                    <h5 className={"text-white text-center"}>Modern Shosh</h5>
-                                </div>
-                                <div className={"d-flex align-items-center"}>
-                                    <a href={"https://www.pinterest.com"} className={"border-0 bg-transparent"}>
-                                        <i className="fa-brands fa-pinterest me-2"
-                                           style={{fontSize: "30px", color: "#ffffff"}}></i>
-                                    </a>
-                                    <a href={"https://www.twitter.com"} className={"border-0 bg-transparent"}>
-                                        <i className="fa-brands fa-twitter me-2"
-                                           style={{fontSize: "30px", color: "#ffffff"}}></i>
-                                    </a>
-                                    <a href={"https://www.facebook.com"} className={"border-0 bg-transparent"}>
-                                        <i className="fa-brands fa-facebook me-2"
-                                           style={{fontSize: "30px", color: "#ffffff"}}></i>
-                                    </a>
-                                    <a href={"https://www.instagram.com"} className={"border-0 bg-transparent"}>
-                                        <i className="fa-brands fa-instagram "
-                                           style={{fontSize: "30px", color: "#ffffff"}}></i>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className={"d-flex flex-column"}>
-                                <a href={"tel:+998951464004"}
-                                   className={"text-decoration-none text-white me-3 mt-3"}>{localStorage.getItem("lang") === "uzb" ? "Telefon raqam" : localStorage.getItem("lang") === "rus" ? "номер телефона" : "Phone Number"}:
-                                    +998951464004</a>
-                                <a href={"https://www.info@modernshosh.uz"}
-                                   className={"text-white text-decoration-none"}>{localStorage.getItem("lang") === "uzb" ? "Email" : localStorage.getItem("lang") === "rus" ? "Электронная почта" : "Email "}:
-                                    info@modernshosh.uz</a>
-                            </div>
-
-                        </div>
-                        <small className={"text-white mt-3"}>2023 © Supersite. All Rights Reserved</small>
+        <div >
+            <div className={"desktop-nav m-0 w-100 row col-md-12"} >
+                <div className={"w-75 d-flex align-items-center"} style={{marginLeft:"200px"}}>
+                    <div className={"col-md-2 mt-3"}>
+                        <a href="#home"><img src={logo} alt="topilmadi" width={"40%"} style={{marginLeft: "80px"}}
+                                             height={"70px"}/>
+                        </a>
                     </div>
-                </div>
-                <div className={"col-md-2 mt-3"}>
-                    <a href="#home"><img src={logo} alt="topilmadi" width={"50%"} height={"100px"}/></a>
+                    <div className={"col-md-8"}>
+                        <div className={"d-flex align-items-center mt-4 flex-column  justify-content-center"}>
+                            <div className={"d-flex align-items center justify-content-center"}>
+                                <div className={"d-flex align-items-center flex-column mt-3"}
+                                     style={{marginRight: "250px"}}>
+                                    <div>
+                                        <h5 className={"text-white text-center"}>Modern Shosh</h5>
+                                    </div>
+                                    <div className={"d-flex align-items-center"}>
+                                        <a href={"https://www.pinterest.com"} className={"border-0 bg-transparent"}>
+                                            <i className="fa-brands fa-pinterest me-2"
+                                               style={{fontSize: "25px", color: "#ffffff"}}></i>
+                                        </a>
+                                        <a href={"https://www.twitter.com"} className={"border-0 bg-transparent"}>
+                                            <i className="fa-brands fa-twitter me-2"
+                                               style={{fontSize: "25px", color: "#ffffff"}}></i>
+                                        </a>
+                                        <a href={"https://www.facebook.com"} className={"border-0 bg-transparent"}>
+                                            <i className="fa-brands fa-facebook me-2"
+                                               style={{fontSize: "25px", color: "#ffffff"}}></i>
+                                        </a>
+                                        <a href={"https://www.instagram.com"} className={"border-0 bg-transparent"}>
+                                            <i className="fa-brands fa-instagram "
+                                               style={{fontSize: "25px", color: "#ffffff"}}></i>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className={"d-flex flex-column mt-4"}>
+                                    <small><a href={"tel:+998951464004"}
+                                              className={"text-decoration-none text-white me-3 mt-3"}>{localStorage.getItem("lang") === "uzb" ? "Telefon raqam" : localStorage.getItem("lang") === "rus" ? "номер телефона" : "Phone Number"}:
+                                        +998951464004</a></small>
+                                   <small className={"mt-2"}> <a href={"https://www.info@modernshosh.uz"}
+                                              className={"text-white text-decoration-none"}>{localStorage.getItem("lang") === "uzb" ? "Email" : localStorage.getItem("lang") === "rus" ? "Электронная почта" : "Email "}:
+                                       info@modernshosh.uz</a></small>
+                                </div>
+
+                            </div>
+                            <small className={"text-white mt-3"}>2023 © Supersite. All Rights Reserved</small>
+                        </div>
+                    </div>
+                    <div className={"col-md-2 mt-3"}>
+                        <a href="#home"><img src={logo} alt="topilmadi" width={"40%"} height={"80px"}/></a>
+                    </div>
+
                 </div>
             </div>
 
