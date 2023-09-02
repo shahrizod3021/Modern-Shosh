516581e23e6cc83744daf30a4d061c700c2d3190
shahrizod3021 <shahrizodmirzaaliyevdev@gmail.com>
2023-09-02 14:02:49 +0500

    39  commit

--- src/Component/News.jsx
+++ src/Component/News.jsx
@@ -46,38 +46,41 @@ export const News = () => {
         <div id={"news"}>
             <div className={"news pt-5 container"}>
                 <h3 className={"text-white text-center"}>{localStorage.getItem("lang") === "uzb" ? "Yangiliklar va bloglar" : localStorage.getItem("lang") === "rus" ? "Новости" : "News and blogs"}</h3>
-                <Carousel responsive={responsive}>
-                    {news.length !== 0 ? (
-                        <>
-                            {news.map((item) => (
-                                <>
-                                    <div className={"col"}>
-                                        <img src={Apis.getContent + item.photoId} draggable={"false"} alt="not found" className={'p-md-2 '} width={"100%"} height={"400vh"}/>
-                                        <p className={'text-white p-md-2'}>{localStorage.getItem("lang") === "uzb" ? item.name : localStorage.getItem("lang") === "rus" ? item.ruName : item.engName}</p>
-                                        <button className={"rounded-0 btn btn-lg btn-outline-warning text-white mb-4"} onClick={() => catching(item.photoId, localStorage.getItem("lang") === "uzb" ? item.uzAbout : localStorage.getItem("lang") === "rus" ? item.ruAbout : item.engAbout, localStorage.getItem("lang") === "uzb" ? item.name : localStorage.getItem("lang") === "rus" ? item.ruName : item.engName,)} type={"button"} data-bs-toggle="modal" data-bs-target="#staticBackdrop" >{localStorage.getItem("lang") === "uzb" ? "Batafsil .... " : localStorage.getItem("lang") === "rus" ? "  Подробно  ...." : "Read more .... "}</button>
-                                    </div>
-                                </>
-                            ))}
-                        </>
-                    ) : (
-                        <>
-                        </>
-                    )}
+                    <Carousel responsive={responsive}>
+                        {news.length !== 0 ? (
+                            news.map((item) => (
+                                    <>
+                                        <div className={"col"}>
+                                            <img src={Apis.getContent + item.photoId} draggable={"false"} alt="not found"
+                                                 className={'p-md-2 '} width={"100%"} height={"400vh"}/>
+                                            <p className={'text-white p-md-2'}>{localStorage.getItem("lang") === "uzb" ? item.name : localStorage.getItem("lang") === "rus" ? item.ruName : item.engName}</p>
+                                            <button className={"rounded-0 btn btn-lg btn-outline-warning text-white mb-4"}
+                                                    onClick={() => catching(item.photoId, localStorage.getItem("lang") === "uzb" ? item.uzAbout : localStorage.getItem("lang") === "rus" ? item.ruAbout : item.engAbout, localStorage.getItem("lang") === "uzb" ? item.name : localStorage.getItem("lang") === "rus" ? item.ruName : item.engName,)}
+                                                    type={"button"} data-bs-toggle="modal"
+                                                    data-bs-target="#staticBackdrop">{localStorage.getItem("lang") === "uzb" ? "Batafsil .... " : localStorage.getItem("lang") === "rus" ? "  Подробно  ...." : "Read more .... "}</button>
+                                        </div>
+                                    </>
+                                ))
 
-                </Carousel>
+                        ) : (
+                           <></>
+                        )}
+                    </Carousel>
             </div>
             <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
                  tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div className="modal-dialog  modal-xl" style={{marginTop:"140px"}}>
-                    <div className="modal-content " style={{backgroundColor:"#202020", height:"auto", borderRadius:"0"}} >
+                <div className="modal-dialog  modal-xl" style={{marginTop: "140px"}}>
+                    <div className="modal-content "
+                         style={{backgroundColor: "#202020", height: "auto", borderRadius: "0"}}>
                         <div className="modal-header">
                             <button type="button" className="bg-transparent text-white border-0" data-bs-dismiss="modal"
                                     aria-label="Close"><i className={"fas fa-x"}></i></button>
                         </div>
-                        <div className="modal-body pt-0 pb-0" style={{paddingLeft:"13px"}}>
+                        <div className="modal-body pt-0 pb-0" style={{paddingLeft: "13px"}}>
                             <div className={"row col-md-12 h-100"}>
                                 <div className={"col-md-6  p-0"}>
-                                    <img src={Apis.getContent + img} className={"w-100"} style={{height:"50vh"}} alt=""/>
+                                    <img src={Apis.getContent + img} className={"w-100"} style={{height: "50vh"}}
+                                         alt=""/>
                                 </div>
                                 <div className={"col-md-5"}>
                                     <h5 className={"text-warning"}>{name}</h5>
